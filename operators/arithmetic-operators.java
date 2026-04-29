98868cfdc748fa84afc1436c77284e9b1a496fdb
vaishnavmansi <vaishnavmansi934@gmail.com>
2026-04-29 21:02:33 +0530

    Added operators problem: Arithmetic operators

---
+++ operators/arithmetic-operators.java
@@ -0,0 +1,25 @@
+import java.util.*;
+// User function Template for Java
+class Solution {
+
+    public static void operations(int x, int y) {
+        // Perform addition x+y below
+        int p =x+y;
+            // Perform subtraction x-y below
+            int q =x-y;
+                // Perform multiplication x*y below
+            int r =x*y;
+                // Perform division x/y below
+            int s =x/y;
+                // Perform modulo operation x%y below
+            int t =x%y;
+
+                // The below code prints the output. Don't change it!
+            System.out.println(p + " " + q + " " + r + " " + s + " " + t);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x =sc.nextInt();
+        int y = sc.nextInt();
+    }
+}
