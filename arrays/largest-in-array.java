3b7092ff791410ce7e939c251acb676053c7b8ec
vaishnavmansi <vaishnavmansi934@gmail.com>
2026-04-29 21:28:27 +0530

    Added arrays problem : Largest in Array

---
+++ arrays/largest-in-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static int largest(int[] arr) {
+        // code here
+        int max=0;
+        for(int i= 0;i<arr.length;i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(largest(arr));
+    }
+}
